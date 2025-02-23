84b7130ad722e6b1ed864b7b0dcfc5df49483f71
mte25 <gk.metehann@gmail.com>
2025-02-23 17:51:31 +0300

    task tablosuna foreign key eklendi

---
+++ database/migrations/2025_02_23_141006_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('category_id');
+            $table->string('title')->nullable();
+            $table->string('content');
+            $table->tinyInteger('status')->default(0)->comment('0-Yapılmadı, 1-Yapılıyor, 2-ertelendi, 3-iptal oldu');
+            $table->dateTime('deadline')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
